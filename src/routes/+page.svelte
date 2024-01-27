3c35e44bf645136f55751d29759bcca9c853d807
divysj <divysen@yandex.com>
2024-01-27 22:59:18 +0530

    Initial Setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<h1 class="text-3xl font-bold underline">
+    Welcome to SvelteKit
+</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.700);
+    }
+    h1{
+        color: rgb(163, 106, 13);
+    } 
+</style>
